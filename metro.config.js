d50da523f9f1d0cfe5723e1db3f2da00bf05f1a5
r12kaushalthota-png <r12.kaushalthota@gmail.com>
2025-09-02 00:13:34 -0400

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
-
-// Ensure compatibility with Expo SDK 53
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
