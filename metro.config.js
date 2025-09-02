0a2543d2e277ba3df746be7d2e679581516a4c62
r12kaushalthota-png <r12.kaushalthota@gmail.com>
2025-09-01 23:23:48 -0400

    Fix Metro resolution error for web-streams-polyfill

--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+
+const config = getDefaultConfig(__dirname);
+
+// Fix web-streams-polyfill resolution
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'web-streams-polyfill/ponyfill/es6': 'web-streams-polyfill/ponyfill/es2018',
+};
+
+module.exports = config;
